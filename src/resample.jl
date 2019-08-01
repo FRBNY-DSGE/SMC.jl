3cb945b1fada914592b2e3a03f91e3210ef382c2
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-01 18:38:05 -0400

    Remove redundancy in resampler

--- src/resample.jl
+++ src/resample.jl
@@ -12,35 +12,33 @@ Reindexing and reweighting samples from a degenerate distribution
         the method for resampling
 
 ### Output:
-
 - `indx`: the newly assigned indices of parameter draws.
 """
 function resample(weights::Vector{Float64}; method::Symbol = :systematic)
+    n_parts = length(weights)
+
     if method == :multinomial
-        n_parts = length(weights)
         indx = Vector{Int64}(undef, n_parts)
 
         # Stores cumulative weights until given index
-        cumulative_weights = cumsum(weights/sum(weights))
+        cumulative_weights = cumsum(weights ./ sum(weights))
         offset = rand(n_parts)
 
-        # This could be parallelized
+        # TODO: parallelize
         for i in 1:n_parts
             indx[i] = findfirst(x -> offset[i] < x, cumulative_weights)
         end
-
         return indx
-    elseif method == :systematic
-        n_parts = length(weights)
 
+    elseif method == :systematic
         # Stores cumulative weights until given index
-        cumulative_weights = cumsum(weights/sum(weights))
+        cumulative_weights = cumsum(weights ./ sum(weights))
         offset = rand()
 
         # Function solves where an individual "spoke" lands
         function subsys(i::Int, offset::Float64, n_parts::Int64, start_ind::Int64,
                         cumulative_weights::Vector{Float64})
-            threshold = (i - 1 + offset)/n_parts
+            threshold = (i - 1 + offset) / n_parts
             range = start_ind:n_parts
             for j in range
                 if cumulative_weights[j] > threshold
@@ -58,13 +56,12 @@ function resample(weights::Vector{Float64}; method::Symbol = :systematic)
                 indx[i] = subsys(i, offset, n_parts, indx[i-1], cumulative_weights)
             end
         end
-
         return indx
+
     elseif method == :polyalgo
-        n_parts = length(weights)
-        weights = Weights(weights./sum(weights))
+        weights = Weights(weights ./ sum(weights))
         return sample(1:n_parts, weights, n_parts, replace = true)
     else
-        throw("Invalid resampler in SMC. Set model setting :resampler_smc to either :systematic, :multinomial, or :polyalgo")
+        throw("Invalid resampler in SMC. Options are :systematic, :multinomial, or :polyalgo")
     end
 end
