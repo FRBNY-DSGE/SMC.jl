e3939a81f59791aaaeb3c351d455469bb153e124
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-02 18:01:15 -0400

    Exporting methods

--- src/SMC.jl
+++ src/SMC.jl
@@ -1,7 +1,7 @@
 isdefined(Base, :__precompile__) && __precompile__()
 
 module SMC
-    using Dates, Test, BenchmarkTools
+    using DSGE, Dates, Test, BenchmarkTools
     using Distributed, Distributions, FileIO, FredData, HDF5, JLD2, LinearAlgebra
     using Missings, Nullables, Printf, Random, RecipesBase, SparseArrays, SpecialFunctions
     using StateSpaceRoutines
@@ -14,7 +14,7 @@ module SMC
     import Base.isempty, Base.<, Base.min, Base.max
     import LinearAlgebra: rank
     import StateSpaceRoutines: KalmanFilter, augment_states_with_shocks
-
+    import DSGE: AbstractModel
     export
         #simulated_annealing, combined_optimizer, lbfgs,
         #filter, filter_shocks, likelihood, posterior, posterior!,
@@ -22,19 +22,15 @@ module SMC
         compute_parameter_covariance, prior, get_estimation_output_files,
         compute_moments, find_density_bands, mutation, resample, smc,
         mvnormal_mixture_draw, nearest_spd, marginal_data_density,
-        initial_draw!, Cloud
+        initial_draw!, Cloud, get_cloud,
 
         # util
-        #@test_matrix_approx_eq, @test_matrix_approx_eq_eps
+        @test_matrix_approx_eq, @test_matrix_approx_eq_eps
 
     const VERBOSITY = Dict(:none => 0, :low => 1, :high => 2)
     const DSGE_DATE_FORMAT = "yymmdd"
     const DSGE_DATASERIES_DELIM = "__"
     const DSGE_SHOCKDEC_DELIM = "__"
-
-    #include("parameters.jl")
-    #include("distributions_ext.jl")
-    #include("util.jl")
 
     #include("benchmark/util.jl")
     #include("benchmark/benchmark.jl")
