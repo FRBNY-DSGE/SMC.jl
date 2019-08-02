5ceec369ed95e6c631ae41343c20a4018d0fd193
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-01 20:13:25 -0400

    Remove references to ParticleCloud

--- src/SMC.jl
+++ src/SMC.jl
@@ -22,7 +22,7 @@ module SMC
         compute_parameter_covariance, prior, get_estimation_output_files,
         compute_moments, find_density_bands, mutation, resample, smc,
         mvnormal_mixture_draw, nearest_spd, marginal_data_density,
-        initial_draw!, ParticleCloud, Particle, Cloud
+        initial_draw!, Cloud
 
         # util
         #@test_matrix_approx_eq, @test_matrix_approx_eq_eps
