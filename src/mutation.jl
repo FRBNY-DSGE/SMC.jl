ce25d4bd9b29a448f88386f7f49a2021d2dbdcf8
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-13 18:02:37 -0400

    Whitespace

--- src/mutation.jl
+++ src/mutation.jl
@@ -39,8 +39,8 @@ function mutation(likelihood::Function, parameters::ParameterVector{U},
                   data::Matrix{S}, p::Vector{S}, d_μ::Vector{S}, d_Σ::Matrix{S},
                   blocks_free::Vector{Vector{Int}}, blocks_all::Vector{Vector{Int}},
                   ϕ_n::S, ϕ_n1::S; c::S = 1., α::S = 1., n_mh_steps::Int = 1,
-                  old_data::T = T(undef, size(data, 1), 0),
-                  verbose::Symbol = :low) where {S<:AbstractFloat, T<:AbstractMatrix, U<:Number}
+                  old_data::T = T(undef, size(data, 1), 0)) where {S<:AbstractFloat,
+                                                                   T<:AbstractMatrix, U<:Number}
 
     n_free_para = length([!θ.fixed for θ in parameters])
     step_prob   = rand() # Draw initial step probability
