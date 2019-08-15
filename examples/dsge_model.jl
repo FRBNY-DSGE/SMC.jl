20cc554efeef5741397fd56e68e218a9fda07332
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-15 18:23:56 -0400

    Trying out SmetsWoutersOrig in example, will change back

--- examples/dsge_model.jl
+++ examples/dsge_model.jl
@@ -1,12 +1,21 @@
 ## Add number of workers of one's choosing:
-#addprocs_frbny(20)
-
-@everywhere using DSGE, ModelConstructors, SMC
+addprocs_frbny(20)
+@everywhere using DSGE, DSGEModels, ModelConstructors, SMC
 @everywhere import ModelConstructors: ParameterVector
 @everywhere import SMC: smc
 
 ## Initialize model object
-m = AnSchorfheide()
+m = SmetsWoutersOrig()
+m <= Setting(:λ, 2.0)
+m <= Setting(:n_particles, 400)
+m <= Setting(:n_Φ, 100)
+m <= Setting(:n_smc_blocks, 1)
+m <= Setting(:n_mh_steps_smc, 1)
+m <= Setting(:use_parallel_workers, true)
+m <= Setting(:resampler_smc, :systematic)
+m <= Setting(:adaptive_tempering_target_smc, false)
+m <= Setting(:use_fixed_schedule, true)
+
 data = df_to_matrix(m, load_data(m))
 
 ## Specify settings
@@ -26,7 +35,11 @@ DSGE.sendto(workers(), m = m)
                     use_chand_recursion = true, verbose = :low)
 end
 
-smc(my_likelihood, m.parameters, data; data_vintage = data_vintage(m),
-    n_parts = 400, n_Φ = 100, λ = 2.0, parallel = true, save_intermediate = true,
-    particle_store_path = particle_store_path, loadpath = loadpath,
-    savepath = savepath)
+# Old SMC: ~9.6 s (12.70 M alloc: 827 MiB)
+@time DSGE.smc(m, data)
+
+# New SMC: ~8.1 s (10.85 M alloc: 673 MiB)
+@time smc(my_likelihood, m.parameters, data; data_vintage = data_vintage(m),
+          n_parts = 400, n_Φ = 100, λ = 2.0, parallel = true, save_intermediate = false,
+          particle_store_path = particle_store_path, loadpath = loadpath,
+          savepath = savepath)
