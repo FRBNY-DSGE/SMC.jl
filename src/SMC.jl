6459c3cb1b939764f3dc22fbd2ea22e7f540e33f
rsarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-16 14:54:25 -0400

    Update and rename SequentialMonteCarlo.jl to SMC.jl

--- src/SMC.jl
+++ src/SMC.jl
@@ -1,6 +1,6 @@
 isdefined(Base, :__precompile__) && __precompile__()
 
-module SequentialMonteCarlo
+module SMC
     using DataFrames
     using Distributed
     using Distributions, Test, BenchmarkTools
