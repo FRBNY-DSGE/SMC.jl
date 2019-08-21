338a259496ce5c63a2ef66cff3852c0a11ab67d1
Ethan Matlin <ethanmatlin@gmail.com>
2019-08-21 15:30:12 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,31 +1,14 @@
 using Documenter, SMC
 
-makedocs(modules = [DSGE],
+makedocs(modules = [SMC],
          clean = false,
          format = Documenter.HTML(),
-         sitename = "DSGE.jl",
+         sitename = "SMC.jl",
          authors = "FRBNY-DSGE",
          linkcheck = false,
          strict = false,
          pages = Any[
                      "Home"                                   => "index.md",
-                     "Model Design"                           => "model_design.md",
-                     "Running An Existing Model"              => "running_existing_model.md",
-                     "Advanced Usage"                         => "advanced_usage.md",
-                     "Input Data"                             => "input_data.md",
-                     "FRBNY Model Input Data"                 => "frbny_data.md",
-                     "Implementation Details"                 => "implementation_details.md",
-                     "Solving the Model"                      => "solving.md",
-                     "Estimation"                             => "estimation.md",
-                     "Forecasting"                            => "forecast.md",
-                     "Computing Means and Bands"              => "means_bands.md",
-                     "Alternative Policies"                   => "altpolicy.md",
-                     "Alternative Scenarios"                  => "scenarios.md",
-                     "Plotting"                               => "plotting.md",
-                     "Algorithms"                             => "algorithms.md",
-                     "Contributing to DSGE.jl"                => "contributing.md",
-                     "MATLAB to Julia Transition: Estimation" => "MatlabToJuliaTransition.md",
-                     "MATLAB to Julia Transition: Forecast"   => "julia_forecasting.md",
                      "License"                                => "license.md"
          ],
          doctest = false # for now
