270a7c7ea17b2ab1450dedd9a0c2dda465c2b3dc
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-21 10:55:47 -0400

    Simplifying example

--- examples/small_dsge_model.jl
+++ examples/small_dsge_model.jl
@@ -1,5 +1,6 @@
 ## Add number of workers of one's choosing:
-#addprocs_frbny(20)
+#addprocs(20)
+
 using DSGE, DSGEModels, ModelConstructors, SMC
 @everywhere using DSGE, DSGEModels, ModelConstructors, SMC
 @everywhere import ModelConstructors: ParameterVector
@@ -26,22 +27,24 @@ filestring_addl     = Vector{String}()
 
 ## Specify output paths
 loadpath            = rawpath(m, "estimate", "smc_cloud.jld2", filestring_addl)
-savepath            = rawpath(m, "estimate", "")
+savepath            = rawpath(m, "estimate", "smc_cloud.jld2", filestring_addl)
 particle_store_path = rawpath(m, "estimate", "smcsave.h5",     filestring_addl)
 
+## Define likelihood function in correct form, on all workers
 DSGE.sendto(workers(), m = m)
-@everywhere function my_likelihood(parameters::ParameterVector, data::Matrix{Float64})::Float64
+function my_loglikelihood(parameters::ParameterVector, data::Matrix{Float64})::Float64
+    DSGE.update!(m, parameters)
+    DSGE.likelihood(m, data; sampler = false, catch_errors = true,
+                    use_chand_recursion = true, verbose = :low)
+end
+@everywhere function my_loglikelihood(parameters::ParameterVector, data::Matrix{Float64})::Float64
     DSGE.update!(m, parameters)
     DSGE.likelihood(m, data; sampler = false, catch_errors = true,
                     use_chand_recursion = true, verbose = :low)
 end
 
-# Old SMC: ~9.6 s (12.70 M alloc: 827 MiB)
-@time DSGE.smc2(m, data)
-@time DSGE.smc(m, data)
-
-# New SMC: ~8.1 s (10.85 M alloc: 673 MiB)
-@time smc(my_likelihood, m.parameters, data; data_vintage = data_vintage(m),
-          n_parts = 400, n_Φ = 100, λ = 2.0, parallel = true, save_intermediate = false,
-          particle_store_path = particle_store_path, loadpath = loadpath,
-          savepath = savepath)
+## Run SMC
+smc(my_loglikelihood, m.parameters, data; verbose = verbose, data_vintage = data_vintage(m),
+    n_parts = 400, n_Φ = 100, λ = 2.0, parallel = true, save_intermediate = false,
+    particle_store_path = particle_store_path, loadpath = loadpath,
+    savepath = savepath)
