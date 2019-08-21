18b67743cde6ed6c9d74c2a773e987c3940b36bc
William Chen <wyc1@williams.edu>
2019-08-21 14:40:12 -0400

    Add files required to push documentation once it is made.

---
+++ docs/README.md
@@ -0,0 +1,4 @@
+# Sequential Monte Carlo (SMC)
+[![Build Status](https://travis-ci.org/FRBNY-DSGE/SMC.jl.svg)](https://travis-ci.org/FRBNY-DSGE/SMC.jl)
+[![](https://img.shields.io/badge/docs-stable-blue.svg)](https://FRBNY-DSGE.github.io/SMC.jl/stable)
+[![](https://img.shields.io/badge/docs-latest-blue.svg)](https://FRBNY-DSGE.github.io/SMC.jl/latest)
---
+++ docs/make.jl
@@ -0,0 +1,44 @@
+using Documenter, SMC
+
+makedocs(modules = [DSGE],
+         clean = false,
+         format = Documenter.HTML(),
+         sitename = "DSGE.jl",
+         authors = "FRBNY-DSGE",
+         linkcheck = false,
+         strict = false,
+         pages = Any[
+                     "Home"                                   => "index.md",
+                     "Model Design"                           => "model_design.md",
+                     "Running An Existing Model"              => "running_existing_model.md",
+                     "Advanced Usage"                         => "advanced_usage.md",
+                     "Input Data"                             => "input_data.md",
+                     "FRBNY Model Input Data"                 => "frbny_data.md",
+                     "Implementation Details"                 => "implementation_details.md",
+                     "Solving the Model"                      => "solving.md",
+                     "Estimation"                             => "estimation.md",
+                     "Forecasting"                            => "forecast.md",
+                     "Computing Means and Bands"              => "means_bands.md",
+                     "Alternative Policies"                   => "altpolicy.md",
+                     "Alternative Scenarios"                  => "scenarios.md",
+                     "Plotting"                               => "plotting.md",
+                     "Algorithms"                             => "algorithms.md",
+                     "Contributing to DSGE.jl"                => "contributing.md",
+                     "MATLAB to Julia Transition: Estimation" => "MatlabToJuliaTransition.md",
+                     "MATLAB to Julia Transition: Forecast"   => "julia_forecasting.md",
+                     "License"                                => "license.md"
+         ],
+         doctest = false # for now
+)
+
+deploydocs(
+    repo = "github.com/FRBNY-DSGE/SMC.jl.git",
+    target = "build",
+    deps = nothing,
+    devbranch = "master",
+    branch = "gh-pages",
+    # versions = "v#",
+    # julia = "0.7",
+    # osname = "osx",
+    make = nothing
+)
