7ad237fa2ac734326568eb742a5df02748d480ab
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-10-01 18:19:06 -0400

    SMC is fully unit tested

--- test/initialization.jl
+++ test/initialization.jl
@@ -3,9 +3,15 @@ write_test_output = false
 path = dirname(@__FILE__)
 
 ###################################################################
-# An Schorfheide
+# Test: initial_draw!()
 ###################################################################
 m = AnSchorfheide()
+
+function loglik_fn(para::ModelConstructors.ParameterVector, data::Matrix{Float64})::Float64
+    DSGE.update!(m, para)
+    DSGE.likelihood(m, data; sampler = false, catch_errors = true,
+               use_chand_recursion = get_setting(m, :use_chand_recursion), verbose = :low)
+end
 
 save = normpath(joinpath(dirname(@__FILE__),"save"))
 m <= Setting(:saveroot, save)
@@ -28,69 +34,114 @@ m <= Setting(:resampling_threshold, .5)
 m <= Setting(:use_fixed_schedule, true)
 
 ####################################################################
-test_init_cloud = ParticleCloud(m, get_setting(m, :n_particles))
+init_cloud = Cloud(length(m.parameters), get_setting(m, :n_particles))
+
 @everywhere Random.seed!(42)
-DSGE.initial_draw!(m, data, test_init_cloud)
+SMC.initial_draw!(loglik_fn, m.parameters, data, init_cloud)
 
 if write_test_output
-    #=JLD.jldopen("reference/initial_draw.jld", "w") do file
-    write(file, "cloud", test_init_cloud)
-    end =#
-    JLD2.jldopen("reference/initial_draw.jld2", "w") do file
-        write(file, "cloud", test_init_cloud)
+    JLD2.jldopen("reference/initial_draw_out.jld2", "w") do file
+        write(file, "cloud", init_cloud)
     end
 end
 
-saved_init_cloud = load("reference/initial_draw.jld2", "cloud")
+saved_init_cloud = load("reference/initial_draw_out.jld2", "cloud")
 
 @testset "Initial draw" begin
-    @test @test_matrix_approx_eq DSGE.get_vals(test_init_cloud) DSGE.get_vals(saved_init_cloud)
-    @test @test_matrix_approx_eq DSGE.get_loglh(test_init_cloud) DSGE.get_loglh(saved_init_cloud)
+    @test @test_matrix_approx_eq SMC.get_vals(init_cloud) SMC.get_vals(saved_init_cloud)
+    @test @test_matrix_approx_eq SMC.get_loglh(init_cloud) SMC.get_loglh(saved_init_cloud)
 end
 
 ###################################################################
-# Smets Wouters
+# Test: one_draw()
 ###################################################################
-#=m = SmetsWoutersOrig()
+m = AnSchorfheide()
 
-save = normpath(joinpath(dirname(@__FILE__),"save"))
-m <= Setting(:saveroot, save)
+file = JLD2.jldopen("reference/one_draw_in.jld2", "r")
+    parameters = file["parameters"]
+    data = file["data"]
+close(file)
 
-data = readdlm("reference/YY.txt")
-data = Matrix{Float64}(data')
-
-m <= Setting(:n_particles, 12000)
-m <= Setting(:n_Φ, 500)
-m <= Setting(:λ, 2.1)
-m <= Setting(:n_smc_blocks, 1)
-m <= Setting(:use_parallel_workers, false)
-m <= Setting(:step_size_smc, 0.4)
-m <= Setting(:n_mh_steps_smc, 1)
-m <= Setting(:resampler_smc, :polyalgo)
-m <= Setting(:target_accept, 0.25)
-
-m <= Setting(:mixture_proportion, 0.9)
-m <= Setting(:tempering_target, 0.95)
-m <= Setting(:resampling_threshold, 0.5)
-m <= Setting(:use_fixed_schedule, true)
-m <= Setting(:use_chand_recursion, true)
-
-####################################################################
-test_init_cloud = ParticleCloud(m, get_setting(m, :n_particles))
-
-@everywhere Random.seed!(42)
-DSGE.initial_draw!(m, data, test_init_cloud)
+draw = SMC.one_draw(loglik_fn, parameters, data)
 
 if write_test_output
-    JLD2.jldopen("reference/initial_draw_sw.jld2", true, true, true, IOStream) do file
-        write(file, "cloud", test_init_cloud)
+    JLD2.jldopen("reference/one_draw_out.jld2", true, true, true, IOStream) do file
+        file["draw"] = draw
     end
 end
 
-saved_init_cloud = load("reference/initial_draw_sw.jld2", "cloud")
-
-@testset "Initial draw" begin
-    @test @test_matrix_approx_eq DSGE.get_vals(test_init_cloud) DSGE.get_vals(saved_init_cloud)
-    @test @test_matrix_approx_eq DSGE.get_loglh(test_init_cloud) DSGE.get_loglh(saved_init_cloud)
+test_draw = JLD2.jldopen("reference/one_draw_out.jld2", "r") do file
+    file["draw"]
 end
-=#
+
+###################################################################
+@testset "One draw" begin
+    @test draw[1] == test_draw[1]
+    @test draw[2] == test_draw[2]
+end
+
+###################################################################
+# Test: draw_likelihood()
+###################################################################
+draw_lik = SMC.draw_likelihood(loglik_fn, parameters, data, vec(draw[1]))
+
+if write_test_output
+    JLD2.jldopen("reference/draw_likelihood_out.jld2", true, true, true, IOStream) do file
+        file["draw_lik"] = draw_lik
+    end
+end
+test_draw_lik = JLD2.jldopen("reference/draw_likelihood_out.jld2", "r") do file
+    file["draw_lik"]
+end
+
+###################################################################
+@testset "Draw Likelihood" begin
+    @test draw_lik[1][1] ≈ test_draw_lik[1][1]
+    @test draw_lik[2] == test_draw_lik[2]
+end
+
+###################################################################
+# Test: initialize_likelihoods!()
+###################################################################
+SMC.initialize_likelihoods!(loglik_fn, parameters, data, init_cloud)
+
+if write_test_output
+    JLD2.jldopen("reference/initialize_likelihood_out.jld2", true, true, true, IOStream) do file
+        file["init_lik_cloud"] = init_cloud
+    end
+end
+test_init_cloud = JLD2.jldopen("reference/initialize_likelihood_out.jld2", "r") do file
+    file["init_lik_cloud"]
+end
+
+###################################################################
+@testset "Initialize Likelihoods" begin
+    @test @test_matrix_approx_eq SMC.get_vals(init_cloud) SMC.get_vals(test_init_cloud)
+    @test @test_matrix_approx_eq SMC.get_loglh(init_cloud) SMC.get_loglh(test_init_cloud)
+end
+
+
+###################################################################
+# Test: initialize_cloud_settings!()
+###################################################################
+SMC.initialize_cloud_settings!(init_cloud)
+
+if write_test_output
+    JLD2.jldopen("reference/initialize_cloud_settings.jld2", true, true, true, IOStream) do file
+        file["init_cloud"] = init_cloud
+    end
+end
+test_init_cloud = JLD2.jldopen("reference/initialize_cloud_settings.jld2", "r") do file
+    file["init_cloud"]
+end
+
+###################################################################
+@testset "Initialize Cloud" begin
+    @test init_cloud.ESS         == test_init_cloud.ESS
+    @test init_cloud.stage_index == test_init_cloud.stage_index
+    @test init_cloud.n_Φ         == test_init_cloud.n_Φ
+    @test init_cloud.resamples   == test_init_cloud.resamples
+    @test init_cloud.c           == test_init_cloud.c
+    @test init_cloud.total_sampling_time == test_init_cloud.total_sampling_time
+    @test init_cloud.tempering_schedule  == test_init_cloud.tempering_schedule
+end
