65c78bed18f693d9c7a36031dd1e042e3ab4f9ea
William Chen <chenwilliamy77@gmail.com>
2020-07-17 10:34:05 -0400

    Fix warning about old and current vintages

--- src/smc_main.jl
+++ src/smc_main.jl
@@ -171,7 +171,9 @@ function smc(loglikelihood::Function, parameters::ParameterVector{U}, data::Matr
 
     # Check that if there's a tempered update, old and current vintages are different
     tempered_update = !isempty(old_data) # Time tempering
-    @warn !(tempered_update & (old_vintage == data_vintage)) "Old & current vintages the same!"
+    if !(tempered_update & (old_vintage == data_vintage))
+        @warn "Old & current vintages the same!"
+    end
 
     # General
     i   = 1             # Index tracking the stage of the algorithm
