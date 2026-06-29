2dc821a561425116a1cd529e926648183548ddec
gargparthb <gargparthb@gmail.com>
2026-06-29 17:33:50 -0400

    renable smc tests

--- test/smc.jl
+++ test/smc.jl
@@ -39,129 +39,129 @@ data = h5read("reference/test_data.h5", "data")
 # the run reproducible and convergent like the standalone sweep.
 Random.seed!(42)
 
-# println("Estimating Linear Model... (approx. 8 minutes)")
+println("Estimating Linear Model... (approx. 8 minutes)")
 
-# # n_mh_steps = 3: a single MH step per tempering stage under-mixes this 9-parameter problem
-# # (the weakest-identified equation, σ1, gets stuck — max|dev| ≈ 0.65 at 1 step, erratic up
-# # to ~48). 3 steps mixes with comfortable margin (the sweep gave ≈ 0.12 at 5 steps).
-# SMC.smc(loglik_fn, m.parameters, data, verbose = :none, use_fixed_schedule = true,
-#         parallel = use_parallel, n_Φ = 120, n_mh_steps = 3, resampling_method = :polyalgo,
-#         data_vintage = "200707", target = 0.25, savepath = savepath,
-#         particle_store_path = particle_store_path, α = .9, threshold_ratio = .5, smc_iteration = 0)
+# n_mh_steps = 3: a single MH step per tempering stage under-mixes this 9-parameter problem
+# (the weakest-identified equation, σ1, gets stuck — max|dev| ≈ 0.65 at 1 step, erratic up
+# to ~48). 3 steps mixes with comfortable margin (the sweep gave ≈ 0.12 at 5 steps).
+SMC.smc(loglik_fn, m.parameters, data, verbose = :none, use_fixed_schedule = true,
+        parallel = use_parallel, n_Φ = 120, n_mh_steps = 3, resampling_method = :polyalgo,
+        data_vintage = "200707", target = 0.25, savepath = savepath,
+        particle_store_path = particle_store_path, α = .9, threshold_ratio = .5, smc_iteration = 0)
 
-# println("Estimation done!")
+println("Estimation done!")
 
-# test_file = load(rawpath(m, "estimate", "smc_cloud.jld2"))
-# test_cloud  = test_file["cloud"]
-# test_w      = test_file["w"]
-# test_W      = test_file["W"]
+test_file = load(rawpath(m, "estimate", "smc_cloud.jld2"))
+test_cloud  = test_file["cloud"]
+test_w      = test_file["w"]
+test_W      = test_file["W"]
 
-# if writing_output
-#     jldopen(string("reference/smc_cloud_fix=true_version=", ver, ".jld2"), true, true, true, IOStream) do file
-#         write(file, "cloud", test_cloud)
-#         write(file, "w", test_w)
-#         write(file, "W", test_W)
-#     end
-# end
+if writing_output
+    jldopen(string("reference/smc_cloud_fix=true_version=", ver, ".jld2"), true, true, true, IOStream) do file
+        write(file, "cloud", test_cloud)
+        write(file, "w", test_w)
+        write(file, "W", test_W)
+    end
+end
 
-# saved_file  = load(string("reference/smc_cloud_fix=true_version=", ver, ".jld2"))
-# saved_cloud = saved_file["cloud"]
-# saved_w     = saved_file["w"]
-# saved_W     = saved_file["W"]
+saved_file  = load(string("reference/smc_cloud_fix=true_version=", ver, ".jld2"))
+saved_cloud = saved_file["cloud"]
+saved_w     = saved_file["w"]
+saved_W     = saved_file["W"]
 
-# ####################################################################
-# cloud_fields = fieldnames(typeof(test_cloud))
-# @testset "Linear Regression Parameter Estimates Are Close" begin
-#     # Posterior mean should be close to true parameters
-#     @test maximum(abs.(mean(SMC.get_vals(test_cloud), dims = 2) -
-#                        [1., 1., 1., 2., 2., 1., 3., 3., 1.])) < .5
-# end
+####################################################################
+cloud_fields = fieldnames(typeof(test_cloud))
+@testset "Linear Regression Parameter Estimates Are Close" begin
+    # Posterior mean should be close to true parameters
+    @test maximum(abs.(mean(SMC.get_vals(test_cloud), dims = 2) -
+                       [1., 1., 1., 2., 2., 1., 3., 3., 1.])) < .5
+end
 
-# @testset "ParticleCloud Fields: Linear" begin
-#     @test @test_matrix_approx_eq SMC.get_vals(test_cloud) SMC.get_vals(saved_cloud)
-#     @test @test_matrix_approx_eq SMC.get_loglh(test_cloud) SMC.get_loglh(saved_cloud)
-#     @test length(test_cloud.particles) == length(saved_cloud.particles)
-#     @test test_cloud.tempering_schedule == saved_cloud.tempering_schedule
-#     @test test_cloud.ESS ≈ saved_cloud.ESS
-#     @test test_cloud.stage_index == saved_cloud.stage_index
-#     @test test_cloud.n_Φ == saved_cloud.n_Φ
-#     @test test_cloud.resamples == saved_cloud.resamples
-#     @test test_cloud.c == saved_cloud.c
-#     @test test_cloud.accept == saved_cloud.accept
-# end
+@testset "ParticleCloud Fields: Linear" begin
+    @test @test_matrix_approx_eq SMC.get_vals(test_cloud) SMC.get_vals(saved_cloud)
+    @test @test_matrix_approx_eq SMC.get_loglh(test_cloud) SMC.get_loglh(saved_cloud)
+    @test length(test_cloud.particles) == length(saved_cloud.particles)
+    @test test_cloud.tempering_schedule == saved_cloud.tempering_schedule
+    @test test_cloud.ESS ≈ saved_cloud.ESS
+    @test test_cloud.stage_index == saved_cloud.stage_index
+    @test test_cloud.n_Φ == saved_cloud.n_Φ
+    @test test_cloud.resamples == saved_cloud.resamples
+    @test test_cloud.c == saved_cloud.c
+    @test test_cloud.accept == saved_cloud.accept
+end
 
-# test_particle  = test_cloud.particles[1,:]
-# saved_particle = saved_cloud.particles[1,:]
-# N = length(test_particle)
-# @testset "Individual Particle Fields Post-SMC: Linear" begin
-#     @test test_particle[1:SMC.ind_para_end(N)] ≈ saved_particle[1:SMC.ind_para_end(N)]
-#     @test test_particle[SMC.ind_loglh(N)]      ≈ saved_particle[SMC.ind_loglh(N)]
-#     @test test_particle[SMC.ind_logprior(N)]   ≈ saved_particle[SMC.ind_logprior(N)]
-#     @test test_particle[SMC.ind_old_loglh(N)] == saved_particle[SMC.ind_old_loglh(N)]
-#     @test test_particle[SMC.ind_accept(N)]    == saved_particle[SMC.ind_accept(N)]
-#     @test test_particle[SMC.ind_weight(N)]     ≈ saved_particle[SMC.ind_weight(N)]
-# end
+test_particle  = test_cloud.particles[1,:]
+saved_particle = saved_cloud.particles[1,:]
+N = length(test_particle)
+@testset "Individual Particle Fields Post-SMC: Linear" begin
+    @test test_particle[1:SMC.ind_para_end(N)] ≈ saved_particle[1:SMC.ind_para_end(N)]
+    @test test_particle[SMC.ind_loglh(N)]      ≈ saved_particle[SMC.ind_loglh(N)]
+    @test test_particle[SMC.ind_logprior(N)]   ≈ saved_particle[SMC.ind_logprior(N)]
+    @test test_particle[SMC.ind_old_loglh(N)] == saved_particle[SMC.ind_old_loglh(N)]
+    @test test_particle[SMC.ind_accept(N)]    == saved_particle[SMC.ind_accept(N)]
+    @test test_particle[SMC.ind_weight(N)]     ≈ saved_particle[SMC.ind_weight(N)]
+end
 
-# @testset "Weight Matrices: Linear" begin
-#     @test @test_matrix_approx_eq test_w saved_w
-#     @test @test_matrix_approx_eq test_W saved_W
-# end
+@testset "Weight Matrices: Linear" begin
+    @test @test_matrix_approx_eq test_w saved_w
+    @test @test_matrix_approx_eq test_W saved_W
+end
 
-# # TODO: add a check here that the correct parameters are estimated
+# TODO: add a check here that the correct parameters are estimated
 
 
-# ####################################################################
-# # Bridging Test
-# ####################################################################
-# include("modelsetup.jl")
-# m = setup_linear_model()
+####################################################################
+# Bridging Test
+####################################################################
+include("modelsetup.jl")
+m = setup_linear_model()
 
-# save = normpath(joinpath(dirname(@__FILE__),"save"))
-# m <= Setting(:saveroot, save)
+save = normpath(joinpath(dirname(@__FILE__),"save"))
+m <= Setting(:saveroot, save)
 
-# data = h5read("reference/test_data.h5", "data")
+data = h5read("reference/test_data.h5", "data")
 
-# @everywhere Random.seed!(42)
+@everywhere Random.seed!(42)
 
-# # Estimate with 1st half of sample
-# m_old = deepcopy(m)
+# Estimate with 1st half of sample
+m_old = deepcopy(m)
 
-# println("Estimating Linear Model on 1st half of sample... (approx. 2 minutes)")
-# SMC.smc(loglik_fn, m_old.parameters, data[:, 1:Int(floor(end/2))], verbose = :none,
-#         use_fixed_schedule = true, parallel = use_parallel,  n_Φ = 100, n_mh_steps = 1,
-#         resampling_method = :polyalgo, data_vintage = "000000", target = 0.25,
-#         savepath = savepath, particle_store_path = particle_store_path, α = .9,
-#         threshold_ratio = .5, smc_iteration = 0, n_parts = 1000)
+println("Estimating Linear Model on 1st half of sample... (approx. 2 minutes)")
+SMC.smc(loglik_fn, m_old.parameters, data[:, 1:Int(floor(end/2))], verbose = :none,
+        use_fixed_schedule = true, parallel = use_parallel,  n_Φ = 100, n_mh_steps = 1,
+        resampling_method = :polyalgo, data_vintage = "000000", target = 0.25,
+        savepath = savepath, particle_store_path = particle_store_path, α = .9,
+        threshold_ratio = .5, smc_iteration = 0, n_parts = 1000)
 
-# m_new = deepcopy(m)
+m_new = deepcopy(m)
 
-# # Estimate with 2nd half of sample
-# m_new <= Setting(:data_vintage, "200708")
-# m_new <= Setting(:tempered_update_prior_weight, 0.5)
-# old_vint = "000000"
-# m_new <= Setting(:previous_data_vintage, old_vint)
-# loadpath = rawpath(m_old, "estimate", "smc_cloud.jld2")
+# Estimate with 2nd half of sample
+m_new <= Setting(:data_vintage, "200708")
+m_new <= Setting(:tempered_update_prior_weight, 0.5)
+old_vint = "000000"
+m_new <= Setting(:previous_data_vintage, old_vint)
+loadpath = rawpath(m_old, "estimate", "smc_cloud.jld2")
 
-# old_cloud = load(loadpath, "cloud")
+old_cloud = load(loadpath, "cloud")
 
-# println("Estimating Linear Model using a bridge distribution... (approx. 2 minutes)")
-# SMC.smc(loglik_fn, m_new.parameters, data, old_data=data[:,1:Int(floor(end/2))], old_cloud=old_cloud,
-#         verbose = :none, use_fixed_schedule = true, parallel = use_parallel,
-#         n_Φ = 100, n_mh_steps = 1, resampling_method = :polyalgo, data_vintage = "200708",
-#         target = 0.25, savepath = savepath, particle_store_path = particle_store_path,
-#         α = .9, threshold_ratio = .5, smc_iteration = 0)
+println("Estimating Linear Model using a bridge distribution... (approx. 2 minutes)")
+SMC.smc(loglik_fn, m_new.parameters, data, old_data=data[:,1:Int(floor(end/2))], old_cloud=old_cloud,
+        verbose = :none, use_fixed_schedule = true, parallel = use_parallel,
+        n_Φ = 100, n_mh_steps = 1, resampling_method = :polyalgo, data_vintage = "200708",
+        target = 0.25, savepath = savepath, particle_store_path = particle_store_path,
+        α = .9, threshold_ratio = .5, smc_iteration = 0)
 
-# loadpath = rawpath(m_new, "estimate", "smc_cloud.jld2")
-# new_cloud = load(loadpath, "cloud")
+loadpath = rawpath(m_new, "estimate", "smc_cloud.jld2")
+new_cloud = load(loadpath, "cloud")
 
-# @testset "Linear Regression Parameter Estimates Are Close" begin
-#     # Posterior mean should be close to true parameters
-#     @test maximum(abs.(mean(SMC.get_vals(test_cloud), dims = 2) - [1., 1., 1., 2., 2., 1., 3., 3., 1.])) < .5
-# end
+@testset "Linear Regression Parameter Estimates Are Close" begin
+    # Posterior mean should be close to true parameters
+    @test maximum(abs.(mean(SMC.get_vals(test_cloud), dims = 2) - [1., 1., 1., 2., 2., 1., 3., 3., 1.])) < .5
+end
 
-# # Clean output files up
-# rm(rawpath(m_new, "estimate", "smc_cloud.jld2"))
-# rm(rawpath(m_new, "estimate", "smcsave.h5"))
+# Clean output files up
+rm(rawpath(m_new, "estimate", "smc_cloud.jld2"))
+rm(rawpath(m_new, "estimate", "smcsave.h5"))
 
 # Benchmark last so re-running smc (which overwrites savepath) can't clobber the cloud the
 # assertions above load from savepath.
